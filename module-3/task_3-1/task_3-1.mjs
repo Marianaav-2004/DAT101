3c737bfce367a64d3ff3688fffbc640fe8d1fbf7
marianaav-2004 <marianaa@uia.no>
2025-10-10 11:02:50 +0200

    oppgaver

--- module-3/task_3-1/task_3-1.mjs
+++ module-3/task_3-1/task_3-1.mjs
@@ -2,41 +2,153 @@
 import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1, 2, 3 ----------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Put your code below here!*/ 
+
 printOut("Task 1, 2 and 3");
-let wakeUpTime = 2;
-if ("Replace this") {
+printOut(newLine);
+
+// Part 1/2; 
+let wakeUpTime = 7; 
+let actuallyWakeUpTime = 6;
+
+  printOut("Part 1/2: ")
+if (wakeUpTime === actuallyWakeUpTime) {
   printOut("I can take the bus to school.");
+} else {
+  printOut(" I need to take the car to school " );
 }
 printOut(newLine);
 
+// Part 3; 
+wakeUpTime = 7; 
+
+printOut (" Part 3: ");
+if (wakeUpTime == 7){
+  printOut (" I can take the bus to school "); 
+} else if (wakeUpTime == 8) {
+  printOut (" I need to take the train to school "); 
+} else {
+  printOut (" I have to take the car to school "); 
+}
+
+
+
+
+
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+
+// Part 4;
+printOut(" Part 4: ");
+let tall = 19; 
+
+if (tall >= 0) {
+  printOut(" Tallet er positivt ");
+} else {
+  printOut(" Tallet er negativt ");
+}
+
+// Part 5;
+printOut (" Part 5: ");
+tall = 0;
+
+if (tall > 0) {
+  printOut (" Tallet er positivt ");
+} else if (tall < 0) {
+  printOut (" Taller er negativt ");
+} else {
+  printOut (" Tallet er null ");
+}
+
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let random = Math.random() * 8;
+let imageSize = 4; 
+if (random > imageSize) {
+  printOut (" Thank you"); 
+} else {
+  printOut (" Too image is too small");
+}
+
 printOut(newLine);
+
+
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let random2 = Math.random() * 8;
+let imageSize2 = 4;
+
+if (random2 > imageSize2) {
+  printOut (" Thank you");
+} else if (random2 >= 6){
+  printOut (" Too image is too large");
+  } else { 
+  printOut (" Too image is too small ");
+  }
+
 printOut(newLine);
 
+
+
+
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+/* Put your code below here!*/ 
+
+const monthList = ["January", "February", "March", "April", "May", "June", "July", 
+  "August", "September", "October", "November", "December"]; 
+const noOfMonth = monthList.length;
+const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
+
+if (monthName === "April" || monthName === "January" || monthName === "February" 
+  || monthName === "March" || monthName === "November" || monthName === "September" 
+  || monthName === "October" || monthName === "December" ){
+    printOut("You must take vitamin D" ); 
+} else if (monthName === "May" || monthName === "June" || monthName === "July" 
+  || monthName === "August" ){
+    printOut("You should not take vitamin D" );
+}
+
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+const mathIndex = monthList.indexOf(monthName);
+printOut(`There are ${daysInMonth[mathIndex]} days in ${monthName}.`);
+
+
 printOut(newLine);
+
+
+
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const monthList2 = ["January", "February", "March", "April", "May", "June", "July", 
+  "August", "September", "October", "November", "December"]; 
+
+const noOfMonth2 = monthList2.length; 
+const monthName2 = monthList2 [Math.floor(Math.random() * noOfMonth2)];
+
+
+
+printOut("In " + monthName2); 
+
+if (monthName2 === "March" || monthName2 === "May") {
+  printOut("The gallery is closed for refurbishment."); 
+} else if (monthName2 === "April") {
+  printOut("The gallery is temporarily open in the building next door");
+} else {
+  printOut("The gallery is open as usual");
+
+}
+
+
+
 printOut(newLine);
--- module-3/task_3-2/task_3-2.mjs
+++ module-3/task_3-2/task_3-2.mjs
@@ -3,38 +3,192 @@ import { printOut, newLine } from "../../common/script/utils.mjs";
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
-printOut(newLine);
+
+let textPart1Line1 = "";
+let textPart1Line2 = "";
+for (let i = 1, j = 10; i <= 10; i++, j--){
+    textPart1Line1 += " " + i;
+    textPart1Line2 += " " + j;
+}
+
+printOut(textPart1Line1);
+printOut(textPart1Line2);
+
+
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const part1GuessNumber = 45;
+let part1Random = Math.floor(Math.random()* 60) + 1;
+while (part1Random !== part1GuessNumber){
+part1Random = Math.floor(Math.random() * 60) + 1; 
+}
+
+printOut("Yes! You guessed correct: " + part1Random);
 printOut(newLine);
+
+
+
+
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+const part3Time = Date.now();
+const part3GuessNumber = 654321;
+let part3Random; 
+let part3Count = 0;
+do{
+    part3Random = Math.floor(Math.random() * 1000000) + 1;
+    part3Count ++;
+}while (part3Random !== part3GuessNumber);
+
+printOut (" Number of guesses: " + part3Count);
+const part3DeltaTime = Date.now() - part3Time; 
+printOut ("Number of milliseconds: " + part3DeltaTime); 
+
 printOut(newLine);
+
+
+
+
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+let textPart4Primes = ""; 
+for (let i = 1; i < 200; i++){
+    let j = i - 1; 
+    let isPrime = true;
+    while (j > 1 && isPrime){
+        let rest = i % j;
+        isPrime = rest != 0; 
+        j--;
+    }
+    if (isPrime) {
+         textPart4Primes += " " + i;
+    }
+   
+} 
+
+printOut(textPart4Primes);
 printOut(newLine);
+
+
+
+
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+
+for (let r = 1; r <= 7; r++){
+    let line = "";
+    for (let c = 1; c <= 9; c++){
+        line = line + "C" + c + "R" + r + " ";
+    }
+
+printOut(line.trim());
+}
+
 printOut(newLine);
+
+
+
+
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+
+function getGrade (score){
+    let percent = (score / 236) * 100;
+    if (percent >= 89) return 'A';
+    else if (percent >= 77) return "B";
+    else if (percent >= 65) return "C";
+    else if (percent >= 53) return "D"; 
+    else if (percent >= 41) return "E";
+    else return "F";
+}  
+for (let student = 1; student <= 5; student++) {
+    let score = Math.floor(Math.random() * 237) + 1;
+    let grade = getGrade(score);
+
+    printOut (" Student " + student + " got score " + score + " and grade " + grade);
+}
+
+
 printOut(newLine);
+
+
+
+
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
-printOut("Replace this with you answer!");
+
+function rolldice (){
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+function getCounts (dice) {
+    let counts = [0, 0, 0, 0, 0, 0];
+    for (let die of dice){
+        counts [die - 1]++;
+    }
+    return counts;
+}
+function checkCombinations(dice) {
+    let counts = getCounts(dice);
+
+    if (counts.every(count => count === 1)) {
+        return "Full straight!";   
+    }
+    else if (counts.filter(count => count === 2).length === 3) {
+        return "3 pairs!";
+    }
+    else if (counts.includes(4) && counts.includes(2)) {
+        return "4 of a kind + a pair!";
+    }
+    else if (counts.includes(6)) {
+        return "Yahtzee!";
+}
+return null;
+}
+
+function trowUntil(target) {
+    let trows = 0;
+    let result = null; 
+
+    do {
+        trows++;
+        let dice =[];
+        for (let i = 0; i < 6; i++) {
+            dice.push(rolldice());
+        }
+        result = checkCombinations(dice);
+    } while (result !== target);
+    return trows;
+}
+let goals = ["Full straight!",
+    "3 pairs!",
+    "4 of a kind + a pair!",
+    "Yahtzee!"];
+for (let goal of goals) {
+    let count = trowUntil(goal);
+    printOut ("It took " + count + " trows to get " + goal);
+}
+
+
+
 printOut(newLine);
+
+
+
+
+
+
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
